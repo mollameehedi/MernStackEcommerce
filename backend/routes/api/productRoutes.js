682a6af82e512766273704f73490264a4f06061d
molla mehedi <mollameehedi@gmail.com>
2024-09-23 19:05:00 +0600

    product add with photo upload by multer

--- backend/routes/api/productRoutes.js
+++ backend/routes/api/productRoutes.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const _ = express.Router()
+
+
 const categoryController = require("../../controllers/categoryController");
 const Category = require("../../model/categoryModel");
 const allCategoryController = require("../../controllers/allCategoryController");
@@ -16,9 +19,24 @@ const productController = require("../../controllers/productController");
 const createStoreController = require("../../controllers/createStoreController");
 const allStoreController = require("../../controllers/allStoreController");
 
+const multer = require("multer");
 
 
-const _ = express.Router()
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+
+
+
 
 _.post('/categorycreate', categoryController)
 _.get('/allcategory',allCategoryController)
@@ -31,8 +49,8 @@ _.post("/deletesubcategory", deleteSubCategoryController);
 _.post("/editsubcategory", editSubCategoryController);
 _.post("/approvecategory", approveCategoryController);
 
-_.post("/products" , productController);
-// _.post("/products", upload.single("avatar"), productController);
+// _.post("/products" , productController);
+_.post("/products", upload.single("avatar"), productController);
 
 _.post("/createstore", createStoreController);
 _.get("/allstore/:id", allStoreController);
